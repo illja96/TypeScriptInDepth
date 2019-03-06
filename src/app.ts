2d62722730926829724ad14b7e8b13af947df157
Illia Khovanskyi <Illia_Khovanskyi@epam.com>
2019-03-06 11:42:26 +0200

    Task 3

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 function getAllBooks(): any[] {
   let books = [
-    { title: 'Refactoring JavaScript', author: 'Evan Burchard', available: true, category: Category.JS },
-    { title: 'JavaScript Testing', author: 'Liang Yuxian Eugene', available: false, category: Category.JS },
-    { title: 'CSS Secrets', author: 'Lea Verou', available: true, category: Category.CSS },
-    { title: 'Mastering JavaScript Object-Oriented Programming', author: 'Andrea Chiarelli', available: true, category: Category.JS }
+    { id: 1, title: 'Refactoring JavaScript', author: 'Evan Burchard', available: true, category: Category.JS },
+    { id: 2, title: 'JavaScript Testing', author: 'Liang Yuxian Eugene', available: false, category: Category.JS },
+    { id: 3, title: 'CSS Secrets', author: 'Lea Verou', available: true, category: Category.CSS },
+    { id: 4, title: 'Mastering JavaScript Object-Oriented Programming', author: 'Andrea Chiarelli', available: true, category: Category.JS }
   ];
 
   return books;
@@ -49,7 +49,25 @@ function logBookTitles(bookTitles: string[]): void {
   console.log(bookTitles);
 }
 
+function getBookById(id: number): any {
+  const books = getAllBooks();
+  const book = books.find((x) => x.id == id);
+
+  return book;
+}
+
+// Task 1
+console.log('Task 1:');
 const books = getAllBooks();
 logFirstAvailable(books);
+
+// Task 2
+console.log('Task 2:');
 const booksCategory = getBookTitlesByCategory(Category.JS);
 logBookTitles(booksCategory);
+
+// Task 3
+console.log('Task 3:');
+books.forEach((x) => console.log(x.title));
+const bookById = getBookById(3);
+console.log(bookById);
