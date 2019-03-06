732dd430050d3ff438224b3d7768af9b9b35011b
Illia Khovanskyi <Illia_Khovanskyi@epam.com>
2019-03-06 11:38:43 +0200

    Task 2

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,9 @@
-const books = getAllBooks();
-logFirstAvailable(books);
-
 function getAllBooks(): any[] {
   let books = [
-    { title: 'Refactoring JavaScript', author: 'Evan Burchard', available: true },
-    { title: 'JavaScript Testing', author: 'Liang Yuxian Eugene', available: false },
-    { title: 'CSS Secrets', author: 'Lea Verou', available: true },
-    { title: 'Mastering JavaScript Object-Oriented Programming', author: 'Andrea Chiarelli', available: true }
+    { title: 'Refactoring JavaScript', author: 'Evan Burchard', available: true, category: Category.JS },
+    { title: 'JavaScript Testing', author: 'Liang Yuxian Eugene', available: false, category: Category.JS },
+    { title: 'CSS Secrets', author: 'Lea Verou', available: true, category: Category.CSS },
+    { title: 'Mastering JavaScript Object-Oriented Programming', author: 'Andrea Chiarelli', available: true, category: Category.JS }
   ];
 
   return books;
@@ -15,7 +12,7 @@ function getAllBooks(): any[] {
 function logFirstAvailable(books: any[]): void {
   console.log(`Books count: ${books.length}`);
 
-  for (let book of books) {
+  for (const book of books) {
     if (!book.available) {
       continue;
     }
@@ -24,3 +21,35 @@ function logFirstAvailable(books: any[]): void {
     break;
   }
 }
+
+enum Category {
+  JS,
+  CSS,
+  HTML,
+  TS,
+  Angular
+}
+
+function getBookTitlesByCategory(category: Category): string[] {
+  const books = getAllBooks();
+  const booksInCategory = new Array<string>();
+
+  for (const book of books) {
+    if (book.category != category) {
+      continue;
+    }
+
+    booksInCategory.push(book.title);
+  }
+
+  return booksInCategory;
+}
+
+function logBookTitles(bookTitles: string[]): void {
+  console.log(bookTitles);
+}
+
+const books = getAllBooks();
+logFirstAvailable(books);
+const booksCategory = getBookTitlesByCategory(Category.JS);
+logBookTitles(booksCategory);
